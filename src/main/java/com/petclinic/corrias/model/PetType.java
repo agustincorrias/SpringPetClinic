874dd03038939dd7aa0a9f1492d40b8855ae0eaa
AgustinCorrias <agustinblascorrias@gmail.com>
2021-09-14 19:04:17 -0300

    Add models

---
+++ src/main/java/com/petclinic/corrias/model/PetType.java
@@ -0,0 +1,16 @@
+package com.petclinic.corrias.model;
+
+public class PetType {
+	
+	private String typeName;
+
+	public String getTypeName() {
+		return typeName;
+	}
+
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
+
+}
+
