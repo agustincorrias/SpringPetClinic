3fe5fa4cc29aa55c4cdec0eda377f66a695fd216
Agustin Corrias <agustinblascorrias@gmail.com>
2021-09-14 21:12:12 -0300

    Add base entity

--- src/main/java/com/petclinic/corrias/model/Pet.java
+++ src/main/java/com/petclinic/corrias/model/Pet.java
@@ -2,7 +2,7 @@ package com.petclinic.corrias.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
 	private LocalDate birthDate;
 	private PetType type;
